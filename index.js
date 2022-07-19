b4956da2774848664104d36f367ac33e903caef1
m-biddle <mbiddle@shastaqa.com>
2022-07-19 09:26:34 -0700

    setup done

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 //48573acc496526da97adfeb6f15a4bdc
 
 //https://www.youtube.com/watch?v=XtMThy8QKqU
+
+
+
+//BELOW is Firebase SDK. Not sure how to utilize yet
+
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//const firebaseConfig = {
+  //apiKey: "AIzaSyD9E3jf_MRwH-Y0_4Q7D38rzcjIDx8Mucg",
+  //authDomain: "netflix-frontend-clone2022.firebaseapp.com",
+  //projectId: "netflix-frontend-clone2022",
+  //storageBucket: "netflix-frontend-clone2022.appspot.com",
+  //messagingSenderId: "571747219038",
+  //appId: "1:571747219038:web:a15cc0469e34be7cb666cf",
+  //measurementId: "G-MSVHLSV6YR"
+//};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
