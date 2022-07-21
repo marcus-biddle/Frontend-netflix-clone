bb306eaf3eeb1307e09e2901003492fdcba6fe72
marcus-biddle <marcusbiddle16@gmail.com>
2022-07-21 16:28:53 -0700

    halfway done

---
+++ netfix-clone/src/Row.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+
+    //code runs based on specific condition/variable
+    useEffect(() => {
+        //if [] blank, run once when loads and dont run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies)
+
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img 
+                    key={movie.id}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`${base_url}${
+                        isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`} 
+                    alt ={movie.name}
+                    
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row
