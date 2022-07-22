6c0efa95590ea8380b7092e1c64c9422c7dd9914
marcus-biddle <marcusbiddle16@gmail.com>
2022-07-22 11:40:59 -0700

    deploy to firebase

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Frontend-netflix-clone
+
+npm install react-scripts
+npm start
+npm i react-youtube
+npm i movie-trailer
+Deploying on firebase
+
+npm install -g firebase-tools
--- netfix-clone/src/Nav.js
+++ netfix-clone/src/Nav.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import "./Nav.css";
 
 function Nav() {
-    const [show, handleShow] = useState(false);
+    /*const [show, handleShow] = useState(false);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -13,17 +13,27 @@ function Nav() {
         return () => {
             window.removeEventListener("scroll");
         };
+    }, []);*/
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
     }, []);
 
     return (
-        <div className ={`nav` /*${show && "nav__black"}`*/}>
+        <div className ={`nav ${show && "nav__black"}`}>
             <img className="nav__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="Netflix Logo"
             />
 
             <img className="nav__avatar"
-            src="https:pbs.twimg.com/profile_images/124011999041155"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
             alt="Netflix Logo"
             />
         </div>
