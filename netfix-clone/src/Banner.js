b67c9535af95569fcdc5860567c3f650cf9130db
marcus-biddle <marcusbiddle16@gmail.com>
2022-07-21 23:03:26 -0700

    frontend skeleton done

--- netfix-clone/src/Banner.js
+++ netfix-clone/src/Banner.js
@@ -38,15 +38,18 @@ function Banner() {
                 <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
-                <div className="banner__button">
+
+                <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
+
                 <h1 className="banner__description">
                     {truncate(movie?.overview, 150)}
                 </h1>
             </div>
 
+            <div className="banner--fadeBottom" />
         </header>
     )
 }
